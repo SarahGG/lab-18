e36e6cd4d015751996dce02e07dd55b741a4447e
SarahGG <guarino.sgg@gmail.com>
2017-05-05 21:10:12 -0400

    implementing better OOP practices

--- scripts/groceryList.js
+++ scripts/groceryList.js
@@ -1,63 +1,28 @@
-window.onload = addElement();
+window.onload = generateList();
 
-function addElement() {
-
+function generateList() {
     window.currentItems = ['Bananas', 'Milk', 'Cereal'];
     window.currentPrices = ['.49', '2.49', '3.99'];
 
     for (var index = 0; index < currentItems.length; index++) {
-        // creating another number
-        var newNum = document.createElement('li');
-        newNum.className = 'list-item';
-        var newNumValue = document.createTextNode(index.toString());
-        newNum.appendChild(newNumValue);
-        document.getElementById('num-list').appendChild(newNum);
-
-        // creating the new item elements
-        var newItem = document.createElement('li');
-        newItem.className = 'list-item';
-        var newItemName = document.createTextNode(currentItems[index]);
-        newItem.appendChild(newItemName);
-        document.getElementById('grocery-items').appendChild(newItem);
-
-        // creating the new price elements
-        var newPrice = document.createElement('li');
-        newPrice.className = 'list-item';
-        var newPriceTotal = document.createTextNode(currentPrices[index]);
-        newPrice.appendChild(newPriceTotal);
-        document.getElementById('grocery-prices').appendChild(newPrice);
+        addRow(index.toString(), currentItems[index],currentPrices[index]);
     }
-
-// <form>
-//     <div class="row section-title skill-subtitle">Enter A New Item</div>
-//         <input class="grocery-text" id="groceryPrice" type="text" placeholder="Grocery Price" title="Grocery Price" />
-//         <input  class="grocery-button" type="button" value="Add It!" onclick="addItem()" />
-//         </form>
 }
 
-function addItem() {
-    // creating another number
-    var nextNum = currentItems.length.toString();
-    var newNum = document.createElement('li');
-    newNum.className = 'list-item';
-    var newNumValue = document.createTextNode(nextNum);
-    newNum.appendChild(newNumValue);
-    document.getElementById('num-list').appendChild(newNum);
+function addToList(id1, id2) {
+    addRow(currentItems.length.toString(), document.getElementById(id1).value, document.getElementById(id2).value);
+}
 
-    // creating the new item elements
-    var newItem = document.createElement('li');
-    newItem.className = 'list-item';
-    var newItemName = document.getElementById('groceryItem').value;
-    var newItemObject = document.createTextNode(newItemName);
-    newItem.appendChild(newItemObject);
-    document.getElementById('grocery-items').appendChild(newItem);
+function addRow (numberValue, groceryValue, priceValue) {
+    addListItem(numberValue, 'num-list');
+    addListItem(groceryValue, 'grocery-items');
+    addListItem(priceValue, 'grocery-prices');
+}
 
-
-    // creating the new price elements
-    var newPrice = document.createElement('li');
-    newPrice.className = 'list-item';
-    var newPriceTotal = document.getElementById('groceryPrice').value;
-    var newPriceObject = document.createTextNode(newPriceTotal);
-    newPrice.appendChild(newPriceObject);
-    document.getElementById('grocery-prices').appendChild(newPrice);
+function addListItem(groceryContent, groceryContainer) {
+    var newLi = document.createElement('li');
+    newLi.className = 'list-item';
+    var newLiValue = document.createTextNode(groceryContent);
+    newLi.appendChild(newLiValue);
+    document.getElementById(groceryContainer).appendChild(newLi);
 }
