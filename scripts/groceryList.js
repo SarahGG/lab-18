617cb67c14bd1a640d4a27c01c245012d7cba9a8
SarahGG <guarino.sgg@gmail.com>
2017-05-08 10:15:30 -0400

    adding 'total cost' components to my shopping list

--- scripts/groceryList.js
+++ scripts/groceryList.js
@@ -2,15 +2,39 @@ window.onload = generateList();
 
 function generateList() {
     window.currentItems = ['Bananas', 'Milk', 'Cereal'];
-    window.currentPrices = ['.49', '2.49', '3.99'];
-
+    window.currentPrices = [.49, 2.49, 3.99];
+    window.priceTotal = 0;
     for (var index = 0; index < currentItems.length; index++) {
-        addRow(index.toString(), currentItems[index],currentPrices[index]);
+        addRow((index+ 1).toString(), currentItems[index],currentPrices[index]);
+        window.priceTotal += parseFloat(currentPrices[index]);
     }
+    addTotal(priceTotal);
 }
 
 function addToList(id1, id2) {
-    addRow(currentItems.length.toString(), document.getElementById(id1).value, document.getElementById(id2).value);
+    // deletes old total row
+    removeLastChild('num-list');
+    removeLastChild('grocery-items');
+    removeLastChild('grocery-prices');
+
+    var oldTotal = window.priceTotal;
+    var addedGrocery = document.getElementById(id1).value;
+    var addedPrice = document.getElementById(id2).value;
+
+    addRow((parseFloat(currentItems.length) + 1).toString(), addedGrocery, addedPrice);
+    var newTotal = parseFloat(oldTotal) + parseFloat(addedPrice);
+
+    addTotal(newTotal);
+}
+
+function removeLastChild(parentElement) {
+    parentElement = document.getElementById(parentElement);
+    var deletedElement = parentElement.lastElementChild;
+    parentElement.removeChild(deletedElement);
+}
+
+function addTotal(totalValue) {
+    addRow(0, 'Total Cost', totalValue);
 }
 
 function addRow (numberValue, groceryValue, priceValue) {
